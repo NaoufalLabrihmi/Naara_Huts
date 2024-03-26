f3ef49be9e8c4fb93d2e93520ef540094dbe1399
Naoufal <naoufal.labrihmi@gmail.com>
2024-03-26 16:08:03 +0000

    make login code clean and add login with phone possiblité

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,126 +2,91 @@
 
 namespace App\Http\Controllers\Auth;
 
-
-use URL;
-use Session;
-use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\App;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Log;
 
 class LoginController extends Controller
 {
-    /*
-        |--------------------------------------------------------------------------
-        | Login Controller
-        |--------------------------------------------------------------------------
-        |
-        | This controller handles authenticating users for the application and
-        | redirecting them to your home screen. The controller uses a trait
-        | to conveniently provide its functionality to your applications.
-        |
-        */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
 
-    /** index page login */
     public function login()
     {
         return view('auth.login');
     }
 
-    /** login with databases */
     public function authenticate(Request $request)
     {
         $request->validate([
-            'email'    => 'required|string',
+            'email_phone' => 'required|string',
             'password' => 'required|string',
         ]);
 
         try {
+            $credentials = $this->getCredentials($request);
 
-            $email     = $request->email;
-            $password  = $request->password;
-
-            if (Auth::attempt(['email' => $email, 'password' => $password], $request->remember)) {
+            if (Auth::attempt($credentials, $request->remember)) {
                 $user = Auth::user();
-                $data = [];
-                $data['response_code']  = '200';
-                $data['status']         = 'success';
-                $data['message']        = 'success Login';
-                if ($user->role === 'admin') {
-                    $data['url']        = '/admin/admin_dahsboard';
-                } elseif ($user->role === 'user') {
-                    $data['url']        = '/';
-                }
-                return response()->json($data);
+                $url = ($user->role === 'admin') ? '/admin/dashboard' : '/';
+
+                return response()->json([
+                    'response_code' => '200',
+                    'status' => 'success',
+                    'message' => 'Success Login',
+                    'url' => $url,
+                ]);
             } else {
-                $data = [];
-                $data['response_code']  = '400';
-                $data['status']         = 'error';
-                $data['message']        = 'fail Login';
-                return response()->json($data);
+                return response()->json([
+                    'response_code' => '400',
+                    'status' => 'error',
+                    'message' => 'Invalid credentials',
+                ]);
             }
         } catch (\Exception $e) {
-            \Log::info($e);
-            DB::rollback();
-            $data = [];
-            $data['response_code']  = '400';
-            $data['status']         = 'error';
-            $data['message']        = 'fail Login';
-            return response()->json($data);
+            Log::error($e);
+            return response()->json([
+                'response_code' => '400',
+                'status' => 'error',
+                'message' => 'Failed to login',
+            ]);
         }
-
-        // $data = [];
-        // $data['response_code']  = '200';
-        // $data['status']         = 'success';
-        // $data['message']        = 'success Login';
-        // $data['info_user']      = $user;
-        // $data['token']          = $accessToken;
-        // return response()->json($data);
     }
 
-    /** logout */
+    protected function getCredentials(Request $request)
+    {
+        $emailOrPhone = $request->input('email_phone');
+        $password = $request->input('password');
+
+        // Check if the input is an email
+        if (filter_var($emailOrPhone, FILTER_VALIDATE_EMAIL)) {
+            return ['email' => $emailOrPhone, 'password' => $password];
+        }
+
+        // Check if the input is a phone number
+        if (preg_match('/^[0-9]+$/', $emailOrPhone)) {
+            return ['phone' => $emailOrPhone, 'password' => $password];
+        }
+
+        // Invalid input format (neither email nor phone)
+        return [];
+    }
+
     public function logout(Request $request)
     {
         Auth::logout();
-        // forget login session
-        $request->session()->forget('name');
-        $request->session()->forget('email');
-        $request->session()->forget('user_id');
-        $request->session()->forget('join_date');
-        $request->session()->forget('last_login');
-        $request->session()->forget('phone_number');
-        $request->session()->forget('status');
-        $request->session()->forget('role_name');
-        $request->session()->forget('avatar');
-        $request->session()->forget('position');
-        $request->session()->forget('department');
-        $request->session()->flush();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
         return redirect('login');
     }
 }
