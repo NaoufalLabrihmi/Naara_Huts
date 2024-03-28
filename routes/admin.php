3c3f0dca02fcdb8c84dad64ae9b25b8e3389ad32
Naoufal <naoufal.labrihmi@gmail.com>
2024-03-28 09:35:54 +0000

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -9,4 +9,5 @@ use Illuminate\Support\Facades\Request;
 Route::middleware(['auth', 'roles:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 });
