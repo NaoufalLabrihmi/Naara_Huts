dc21f617594240ed3ae778bf82ca054efc54331f
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-05 02:08:16 +0000

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,7 +38,7 @@ class LoginController extends Controller
 
             if (Auth::attempt($credentials, $request->remember)) {
                 $user = Auth::user();
-                $url = ($user->role === 'admin') ? '/admin/dashboard' : '/';
+                $url = ($user->role === 'admin') ? '/admin/dashboard' : '/dashboard';
 
                 return response()->json([
                     'response_code' => '200',
