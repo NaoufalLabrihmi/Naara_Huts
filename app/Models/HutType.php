92d2424a009ce3a740b513ae6745584d32a956f8
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-06 17:16:21 +0000

    Create HutType.php

---
+++ app/Models/HutType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HutType extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
