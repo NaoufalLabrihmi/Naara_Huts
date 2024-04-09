46b5cc7d4f9b58aaee00a32ca812897d42afdc23
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-09 18:50:47 +0000

    Update FrontendHutController.php

--- app/Http/Controllers/Frontend/FrontendHutController.php
+++ app/Http/Controllers/Frontend/FrontendHutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Frontend;
 use App\Models\Hut;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Facility;
+use App\Models\MultiImage;
 
 class FrontendHutController extends Controller
 {
@@ -12,5 +14,14 @@ class FrontendHutController extends Controller
     {
         $huts = Hut::latest()->get();
         return view('frontend.hut.all_huts', compact('huts'));
+    }
+
+    public function HutDetailsPage($id)
+    {
+        $hutdetails = Hut::find($id);
+        $multiImage = MultiImage::where('huts_id', $id)->get();
+        $facility = Facility::where('huts_id', $id)->get();
+        $otherHuts = Hut::where('id', '!=', $id)->orderBy('id', 'DESC')->limit(2)->get();
+        return view('frontend.hut.hut_details', compact('hutdetails', 'multiImage', 'facility', 'otherHuts'));
     }
 }
