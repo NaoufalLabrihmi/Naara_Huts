6301c586104356f0df0249b374c9a1d82a00d8e3
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-17 17:15:06 +0100

    Create add_hutlist.blade.php

---
+++ resources/views/backend/allhut/hutlist/add_hutlist.blade.php
@@ -0,0 +1,166 @@
+@extends('admin.admin_dashboard')
+@section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+<div class="page-content">
+    <!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+        <div class="breadcrumb-title pe-3">Add Hut List</div>
+        <div class="ps-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 p-0">
+                    <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">Add Hut List</li>
+                </ol>
+            </nav>
+        </div>
+
+    </div>
+    <!--end breadcrumb-->
+    <div class="container">
+        <div class="main-body">
+            <div class="row">
+
+                <div class="col-lg-12">
+
+                    <div class="card">
+                        <div class="card-body p-4">
+
+
+                            <form method="POST" action="{{ route('store.hutlist') }}" class="row g-3">
+                                @csrf
+                                <div class="col-md-4">
+                                    <label for="huttype_id" class="form-label">Hut Type</label>
+                                    <select name="hut_id" id="hut_id" class="form-select">
+                                        <option selected="">Select Hut Type </option>
+                                        @foreach ($huttype as $item)
+                                        <option value="{{ $item->hut->id }}" {{ collect(old('huttype_id'))->contains($item->id) ? 'selected' : '' }}>{{ $item->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-md-4">
+                                    <label for="input2" class="form-label">Checkin</label>
+                                    <input type="date" name="check_in" class="form-control" id="check_in">
+                                </div>
+
+                                <div class="col-md-4">
+                                    <label for="input2" class="form-label">CheckOut</label>
+                                    <input type="date" name="check_out" class="form-control" id="check_out">
+                                </div>
+
+
+
+                                <div class="col-md-4">
+                                    <label for="input3" class="form-label">Hut</label>
+                                    <input type="number" name="number_of_huts" class="form-control">
+                                    <input type="hidden" name="available_hut" id="available_hut" class="form-control">
+
+                                    <div class="mt-2">
+                                        <label for="">Availability <span class="text-success availability"></span> </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <label for="input4" class="form-label">Guest</label>
+                                    <input type="text" name="number_of_person" class="form-control" id="number_of_person">
+                                </div>
+                                <h3 class="mt-3 mb-5 text-center">Customer Information </h3>
+
+                                <div class="col-md-4">
+                                    <label for="input5" class="form-label">Name</label>
+                                    <input type="text" name="name" class="form-control" id="input5" value="{{ old('name') }}">
+                                </div>
+                                <div class="col-md-4">
+                                    <label for="input5" class="form-label">Email</label>
+                                    <input type="email" name="email" class="form-control" value="{{ old('email') }}">
+                                </div>
+                                <div class="col-md-4">
+                                    <label for="input5" class="form-label">Phone</label>
+                                    <input type="text" name="phone" class="form-control" value="{{ old('phone') }}">
+                                </div>
+
+                                <div class="col-md-4">
+                                    <label for="input5" class="form-label">Country</label>
+                                    <input type="text" name="country" class="form-control" value="{{ old('country') }}">
+                                </div>
+                                <div class="col-md-4">
+                                    <label for="input5" class="form-label">Zip Code</label>
+                                    <input type="text" name="zip_code" class="form-control" value="{{ old('zip_code') }}">
+                                </div>
+                                <div class="col-md-4">
+                                    <label for="input5" class="form-label">State</label>
+                                    <input type="text" name="state" class="form-control" value="{{ old('state') }}">
+                                </div>
+                                <div class="col-md-12">
+                                    <label for="input11" class="form-label">Address</label>
+                                    <textarea name="address" class="form-control" rows="3">{{ old('address') }}</textarea>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="d-md-flex d-grid align-items-center gap-3">
+                                        <button type="submit" class="btn btn-primary px-4">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    $(document).ready(function() {
+        $("#hut_id").on('change', function() {
+            $("#check_in").val('');
+            $("#check_out").val('');
+            $(".availability").text(0);
+            $("#available_hut").val(0);
+        });
+        $("#check_out").on('change', function() {
+            getAvaility();
+        });
+    });
+
+    function getAvaility() {
+        var check_in = $('#check_in').val();
+        var check_out = $('#check_out').val();
+        var hut_id = $("#hut_id").val();
+        var startDate = new Date(check_in);
+        var endDate = new Date(check_out);
+        if (startDate > endDate) {
+            alert('Invalid Date');
+            $("#check_out").val('');
+            return false;
+        }
+        if (check_in != '' && check_out != '' && hut_id != '') {
+            $.ajax({
+                url: "{{ route('check_hut_availability') }}",
+                data: {
+                    hut_id: hut_id,
+                    check_in: check_in,
+                    check_out: check_out
+                },
+                success: function(data) {
+                    $(".availability").text(data['available_hut']);
+                    $("#available_hut").val(data['available_hut']);
+                }
+            });
+
+        } else {
+            alert('Field must be not empty')
+        }
+
+    }
+</script>
+
+
+
+
+
+
+@endsection
