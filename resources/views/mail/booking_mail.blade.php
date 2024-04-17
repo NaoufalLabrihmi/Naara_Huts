4605d3bc19eaabfa35c4a42051065141471a900f
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-17 22:37:38 +0100

    cofirmWithMail

---
+++ resources/views/mail/booking_mail.blade.php
@@ -0,0 +1,137 @@
+<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+    <!-- START HEADER/BANNER -->
+
+    <tbody>
+        <tr>
+            <td align="center">
+                <table class="col-600" width="600" border="0" align="center" cellpadding="0" cellspacing="0">
+                    <tbody>
+                        <tr>
+                            <td align="center" valign="top" background="https://www.inaracamp.com/wp-content/uploads/2020/09/yazgg.jpg" bgcolor="#66809b" style="background-size: cover; background-position: top" height="200">
+                                <table class="col-600" width="600" height="200" border="0" align="center" cellpadding="0" cellspacing="0">
+                                    <tbody>
+                                        <tr>
+                                            <td height="40"></td>
+                                        </tr>
+
+                                        <tr>
+                                            <td align="center" style="line-height: 0px">
+                                                <img style="
+                            display: block;
+                            line-height: 0px;
+                            font-size: 0px;
+                            border: 0px;
+                          " src="https://images.squarespace-cdn.com/content/v1/5f997ef94da0c64db54c7a8f/1606126031006-2Q0O27XG4KGW3N3IKELQ/logo_SCARABEOCAMP_black.png?format=1500w" width="109" height="103" alt="logo" />
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td align="center" style="
+                          font-family: 'Raleway', sans-serif;
+                          font-size: 37px;
+                          color: #ffffff;
+                          line-height: 24px;
+                          font-weight: bold;
+                          letter-spacing: 7px;
+                        ">
+                                                SCARABEO
+                                                <span style="
+                            font-family: 'Raleway', sans-serif;
+                            font-size: 37px;
+                            color: #ffffff;
+                            line-height: 39px;
+                            font-weight: 300;
+                            letter-spacing: 7px;
+                          ">-CAMP</span>
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td align="center" style="
+                          font-family: 'Lato', sans-serif;
+                          font-size: 15px;
+                          color: #ffffff;
+                          line-height: 24px;
+                          font-weight: 300;
+                        ">
+                                                Un voyage hors du temps.
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td height="50"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+
+        <!-- END HEADER/BANNER -->
+
+        <!-- START 3 BOX SHOWCASE -->
+
+        <tr>
+            <td align="center">
+                <table class="col-600" width="600" border="0" align="center" cellpadding="0" cellspacing="0" style="
+            margin-left: 20px;
+            margin-right: 20px;
+            border-left: 1px solid #dbd9d9;
+            border-right: 1px solid #dbd9d9;
+          ">
+                    <tbody>
+                        <tr>
+                            <td height="35"></td>
+                        </tr>
+
+                        <tr>
+                            <td align="center" style="
+                  font-family: 'Raleway', sans-serif;
+                  font-size: 22px;
+                  font-weight: bold;
+                  color: #2a3a4b;
+                ">
+                                Your Request is confirm on this data.
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td height="10"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+
+        <tr>
+            <td align="center">
+                <table class="col-600" width="600" border="0" align="center" cellpadding="0" cellspacing="0" style="
+            border-left: 1px solid #dbd9d9;
+            border-right: 1px solid #dbd9d9;
+          ">
+                    <tbody>
+                        <tr>
+                            <td height="10"></td>
+                        </tr>
+                        <tr>
+                            <td style="text-align: center">
+                                <h4>check in : {{ $booking['check_in'] }}</h4>
+                                <h4>check Out : {{ $booking['check_out'] }}</h4>
+                                <h4>User Name: {{ $booking['name'] }}</h4>
+                                <h4>Email : {{ $booking['email'] }}</h4>
+                                <h4>Phone : {{ $booking['phone'] }}</h4>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+
+        <tr>
+            <td height="5"></td>
+        </tr>
+    </tbody>
+</table>
