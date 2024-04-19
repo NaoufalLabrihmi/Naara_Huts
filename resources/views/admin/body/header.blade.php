e425e17901939e4d9f42a9dca751db0d41c73e8d
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-19 05:15:37 +0100

    Update header.blade.php

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -25,86 +25,39 @@
 
 
                     <li class="nav-item dropdown dropdown-large">
-                        <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="#" data-bs-toggle="dropdown"><span class="alert-count">7</span>
-                            <i class='bx bx-bell'></i>
-                        </a>
+                        <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="#" data-bs-toggle="dropdown">
+                            @php
+                            $ncount = Auth::user()->unreadNotifications()->count();
+                            @endphp
+                            <span class="alert-count" id="notification-count">{{ $ncount }}</span>
+                            <i class='bx bx-bell'></i></a>
                         <div class="dropdown-menu dropdown-menu-end">
                             <a href="javascript:;">
                                 <div class="msg-header">
                                     <p class="msg-header-title">Notifications</p>
-                                    <p class="msg-header-badge">8 New</p>
+                                    <p class="msg-header-badge"></p>
                                 </div>
                             </a>
                             <div class="header-notifications-list">
-                                <a class="dropdown-item" href="javascript:;">
+                                @php
+                                $user = Auth::user();
+                                @endphp
+
+                                @forelse ($user->notifications as $notification)
+
+                                <a class="dropdown-item" href="javascript:;" onclick="markNotificationAsRead('{{ $notification->id }}')">
                                     <div class="d-flex align-items-center">
-                                        <div class="user-online">
-                                            <img src="backend/assets/images/avatars/avatar-1.png" class="msg-avatar" alt="user avatar">
+                                        <div class="notify bg-light-success text-success"><i class='bx bx-check-square'></i>
                                         </div>
                                         <div class="flex-grow-1">
-                                            <h6 class="msg-name">Daisy Anderson<span class="msg-time float-end">5 sec
-                                                    ago</span></h6>
-                                            <p class="msg-info">The standard chunk of lorem</p>
+                                            <h6 class="msg-name">{{ $notification->data['message'] }}<span class="msg-time float-end"> {{ Carbon\Carbon::parse($notification->created_at)->diffForHumans() }} </span></h6>
+                                            <p class="msg-info">New Booking </p>
                                         </div>
                                     </div>
                                 </a>
-                                <a class="dropdown-item" href="javascript:;">
-                                    <div class="d-flex align-items-center">
-                                        <div class="notify bg-light-danger text-danger">dc
-                                        </div>
-                                        <div class="flex-grow-1">
-                                            <h6 class="msg-name">New Orders <span class="msg-time float-end">2 min
-                                                    ago</span></h6>
-                                            <p class="msg-info">You have recived new orders</p>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item" href="javascript:;">
-                                    <div class="d-flex align-items-center">
-                                        <div class="user-online">
-                                            <img src="backend/assets/images/avatars/avatar-2.png" class="msg-avatar" alt="user avatar">
-                                        </div>
-                                        <div class="flex-grow-1">
-                                            <h6 class="msg-name">Althea Cabardo <span class="msg-time float-end">14
-                                                    sec ago</span></h6>
-                                            <p class="msg-info">Many desktop publishing packages</p>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item" href="javascript:;">
-                                    <div class="d-flex align-items-center">
-                                        <div class="notify bg-light-success text-success">
-                                            <img src="backend/assets/images/app/outlook.png" width="25" alt="user avatar">
-                                        </div>
-                                        <div class="flex-grow-1">
-                                            <h6 class="msg-name">Account Created<span class="msg-time float-end">28 min
-                                                    ago</span></h6>
-                                            <p class="msg-info">Successfully created new email</p>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item" href="javascript:;">
-                                    <div class="d-flex align-items-center">
-                                        <div class="notify bg-light-info text-info">Ss
-                                        </div>
-                                        <div class="flex-grow-1">
-                                            <h6 class="msg-name">New Product Approved <span class="msg-time float-end">2 hrs ago</span></h6>
-                                            <p class="msg-info">Your new product has approved</p>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item" href="javascript:;">
-                                    <div class="d-flex align-items-center">
-                                        <div class="user-online">
-                                            <img src="backend/assets/images/avatars/avatar-4.png" class="msg-avatar" alt="user avatar">
-                                        </div>
-                                        <div class="flex-grow-1">
-                                            <h6 class="msg-name">Katherine Pechon <span class="msg-time float-end">15
-                                                    min ago</span></h6>
-                                            <p class="msg-info">Making this the first true generator</p>
-                                        </div>
-                                    </div>
-                                </a>
+                                @empty
+
+                                @endforelse
                             </div>
                             <a href="javascript:;">
                                 <div class="text-center msg-footer">
@@ -262,3 +215,23 @@
         </nav>
     </div>
 </header>
+
+<script>
+    function markNotificationAsRead(notificationId) {
+        fetch('/mark-notification-as-read/' + notificationId, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+                body: JSON.stringify({})
+            })
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('notification-count').textContent = data.count;
+            })
+            .catch(error => {
+                console.log('Error', error);
+            });
+    }
+</script>
