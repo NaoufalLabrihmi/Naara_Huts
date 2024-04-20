55b31fb4c020c7c3c9ce034aa3bc01dfcdc25874
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-20 20:16:20 +0100

    Update header.blade.php

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -6,13 +6,7 @@ $setting = App\Models\SiteSetting::find(1);
         <div class="row align-items-center">
             <div class="col-lg-3 col-md-2 pr-0">
                 <div class="language-list">
-                    <select class="language-list-item">
-                        <option>English</option>
-                        <option>العربيّة</option>
-                        <option>Deutsch</option>
-                        <option>Português</option>
-                        <option>简体中文</option>
-                    </select>
+
                 </div>
             </div>
 
