7388d4b87ca122c5bb1a831836a6caee4df05e71
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-20 20:16:27 +0100

    Update show_gallery.blade.php

--- resources/views/frontend/gallery/show_gallery.blade.php
+++ resources/views/frontend/gallery/show_gallery.blade.php
@@ -17,43 +17,78 @@
     </div>
 </div>
 <!-- Inner Banner End -->
+<style>
+    /* Gallery Overlay */
+    .gallery-item {
+        position: relative;
+        overflow: hidden;
+    }
+
+    .gallery-item img {
+        transition: transform 0.3s ease;
+    }
+
+    .gallery-item:hover img {
+        transform: scale(1.1);
+    }
+
+    .gallery-overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, 0.7);
+        opacity: 0;
+        transition: opacity 0.3s ease;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .gallery-item:hover .gallery-overlay {
+        opacity: 1;
+    }
+
+    .gallery-icon {
+        color: #fff;
+        font-size: 36px;
+        text-decoration: none;
+        transition: transform 0.3s ease;
+    }
+
+    .gallery-icon:hover {
+        transform: scale(1.2);
+    }
+</style>
 
 <!-- Gallery Area -->
 <div class="gallery-area pt-100 pb-70">
     <div class="container">
-        <div class="tab gallery-tab">
+        <div class="gallery-view">
+            <div class="row justify-content-center">
 
 
-            <div class="tab_content current active pt-45">
-                <div class="tabs_item current">
-                    <div class="gallery-tab-item">
-                        <div class="gallery-view">
-                            <div class="row">
-
-                                @foreach ($gallery as $item)
-                                <div class="col-lg-4 col-sm-6">
-                                    <div class="single-gallery">
-                                        <img src="{{ asset($item->photo_name) }}" alt="Images">
-                                        <a href="{{ asset($item->photo_name) }}" class="gallery-icon">
-                                            <i class='bx bx-plus'></i>
-                                        </a>
-                                    </div>
-                                </div>
-                                @endforeach
-
-
-                            </div>
+                @foreach ($gallery as $item)
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+                    <div class="gallery-item">
+                        <img src="{{ asset($item->photo_name) }}" class="img-fluid" alt="Gallery Image">
+                        <div class="gallery-overlay">
+                            <a href="{{ asset($item->photo_name) }}" class="gallery-icon">
+                                <i class="bx bx-plus"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
-
-
+                @endforeach
 
 
             </div>
         </div>
     </div>
 </div>
+
+
 <!-- Gallery Area End -->
 
 
