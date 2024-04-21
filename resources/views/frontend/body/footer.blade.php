23628c5fa56eab73496d92a3a6ef1d4a30de0b18
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-21 23:46:02 +0100

    Update footer.blade.php

--- resources/views/frontend/body/footer.blade.php
+++ resources/views/frontend/body/footer.blade.php
@@ -1,186 +1,146 @@
 @php
 $setting = App\Models\SiteSetting::find(1);
 @endphp
-<footer class="footer-area footer-bg">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+
+
+    .container {
+        box-sizing: border-box;
+        line-height: 1.5;
+        font-family: 'Poppins', sans-serif;
+        max-width: 1170px;
+        margin: auto;
+    }
+
+    .row {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    .footer {
+        background-color: #24262b;
+        padding: 70px 0;
+    }
+
+    .footer-col {
+        width: 25%;
+        padding: 0 15px;
+    }
+
+    .footer-col h4 {
+        font-size: 18px;
+        color: #ffffff;
+        text-transform: capitalize;
+        margin-bottom: 35px;
+        font-weight: 500;
+        position: relative;
+    }
+
+    .footer-col h4::before {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: -10px;
+        background-color: #595f39;
+        height: 2px;
+        box-sizing: border-box;
+        width: 50px;
+    }
+
+    .footer-col ul li:not(:last-child) {
+        margin-bottom: 10px;
+    }
+
+    .footer-col ul li a {
+        font-size: 16px;
+        text-transform: capitalize;
+        color: #ffffff;
+        text-decoration: none;
+        font-weight: 300;
+        color: #bbbbbb;
+        display: block;
+        transition: all 0.3s ease;
+    }
+
+    .footer-col ul li a:hover {
+        color: #ffffff;
+        padding-left: 8px;
+    }
+
+    .footer-col .social-links a {
+        display: inline-block;
+        height: 40px;
+        width: 40px;
+        background-color: rgba(255, 255, 255, 0.2);
+        margin: 0 10px 10px 0;
+        text-align: center;
+        line-height: 40px;
+        border-radius: 50%;
+        color: #ffffff;
+        transition: all 0.5s ease;
+    }
+
+    .footer-col .social-links a:hover {
+        color: #24262b;
+        background-color: #ffffff;
+    }
+
+    /*responsive*/
+    @media(max-width: 767px) {
+        .footer-col {
+            width: 50%;
+            margin-bottom: 30px;
+        }
+    }
+
+    @media(max-width: 574px) {
+        .footer-col {
+            width: 100%;
+        }
+    }
+</style>
+<footer class="footer">
     <div class="container">
-        <div class="footer-top pt-100 pb-70">
-            <div class="row align-items-center">
-                <div class="col-lg-3 col-md-6">
-                    <div class="footer-widget">
-                        <div class="footer-logo">
-                            <a href="index.html">
-                                <img src="{{ asset($setting->logo) }}" alt="Images">
-                            </a>
-                        </div>
-                        <p>
-                            Aenean finibus convallis nisl sit amet hendrerit. Etiam blandit velit non lorem mattis, non ultrices eros bibendum .
-                        </p>
-                        <ul class="footer-list-contact">
-                            <li>
-                                <i class='bx bx-home-alt'></i>
-                                <a href="#">{{ $setting->address }}</a>
-                            </li>
-                            <li>
-                                <i class='bx bx-phone-call'></i>
-                                <a href="tel:{{ $setting->phone }}">{{ $setting->phone }}</a>
-                            </li>
-                            <li>
-                                <i class='bx bx-envelope'></i>
-                                <a href="mailto:{{$setting->email}}">{{$setting->email}}</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="footer-widget pl-5">
-                        <h3>Links</h3>
-                        <ul class="footer-list">
-                            <li>
-                                <a href="about.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    About Us
-                                </a>
-                            </li>
-                            <li>
-                                <a href="services-1.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    Services
-                                </a>
-                            </li>
-                            <li>
-                                <a href="team.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    Team
-                                </a>
-                            </li>
-                            <li>
-                                <a href="gallery.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    Gallery
-                                </a>
-                            </li>
-                            <li>
-                                <a href="terms-condition.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    Terms
-                                </a>
-                            </li>
-                            <li>
-                                <a href="privacy-policy.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    Privacy Policy
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="footer-widget">
-                        <h3>Useful Links</h3>
-                        <ul class="footer-list">
-                            <li>
-                                <a href="index.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    Home
-                                </a>
-                            </li>
-                            <li>
-                                <a href="blog-1.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    Blog
-                                </a>
-                            </li>
-                            <li>
-                                <a href="faq.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    FAQ
-                                </a>
-                            </li>
-                            <li>
-                                <a href="testimonials.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    Testimonials
-                                </a>
-                            </li>
-                            <li>
-                                <a href="gallery.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    Gallery
-                                </a>
-                            </li>
-                            <li>
-                                <a href="contact.html" target="_blank">
-                                    <i class='bx bx-caret-right'></i>
-                                    Contact Us
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <!-- <div class="col-lg-3 col-md-6">
-                    <div class="footer-widget">
-                        <h3>Newsletter</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Nullam tempor eget ante fringilla rutrum aenean sed venenatis .
-                        </p>
-                        <div class="footer-form">
-                            <form class="newsletter-form" data-toggle="validator" method="POST">
-                                <div class="row">
-                                    <div class="col-lg-12">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control" placeholder="Your Email*" name="EMAIL" required autocomplete="off">
-                                        </div>
-                                    </div>
-
-                                    <div class="col-lg-12 col-md-12">
-                                        <button type="submit" class="default-btn btn-bg-one">
-                                            Subscribe Now
-                                        </button>
-                                        <div id="validator-newsletter" class="form-result"></div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div> -->
+        <div class="row">
+            <div class="footer-col">
+                <h4>SCARABEO</h4>
+                <ul>
+                    <li><a href="#">{{ $setting->address }}</a></li>
+                    <li><a href="tel:{{ $setting->phone }}">{{ $setting->phone }}</a></li>
+                    <li><a href="mailto:{{$setting->email}}">{{$setting->email}}</a></li>
+                </ul>
             </div>
-        </div>
-
-        <div class="copy-right-area">
-            <div class="row">
-                <div class="col-lg-8 col-md-8">
-                    <div class="copy-right-text text-align1">
-                        <p>
-                            {{ $setting->copyright }}
-
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-4">
-                    <div class="social-icon text-align2">
-                        <ul class="social-link">
-                            <li>
-                                <a href="{{ $setting->facebook }}" target="_blank"><i class='bx bxl-facebook'></i></a>
-                            </li>
-                            <li>
-                                <a href="{{ $setting->twitter }}" target="_blank"><i class='bx bxl-twitter'></i></a>
-                            </li>
-                            <li>
-                                <a href="#" target="_blank"><i class='bx bxl-instagram'></i></a>
-                            </li>
-                            <li>
-                                <a href="#" target="_blank"><i class='bx bxl-pinterest-alt'></i></a>
-                            </li>
-                            <li>
-                                <a href="#" target="_blank"><i class='bx bxl-youtube'></i></a>
-                            </li>
-                        </ul>
-                    </div>
+            <div class="footer-col">
+                <h4>get help</h4>
+                <ul>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">shipping</a></li>
+                    <li><a href="#">returns</a></li>
+                    <li><a href="#">order status</a></li>
+                    <li><a href="#">payment options</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Our Content</h4>
+                <ul>
+                    <li><a href="{{route('froom.all')}}">Huts</a></li>
+                    <li><a href="{{route('show.gallery')}}">Gallery</a></li>
+                    <li><a href="{{route('contact.us')}}">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>follow us</h4>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
         </div>
