b45354cb3b0a456351dcdc81e34c6db6211a51e0
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-21 22:48:03 +0100

    Create global.php

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php
+
+
+return [
+    'permissions' => [
+        'dashboard' => 'See Dashboard',
+        'team' => 'Manage Team',
+        'bookarea' => 'Update Book Area',
+        'huts' => 'Manage Huts',
+        'booking' => 'Manage Booking',
+        'hutlist' => 'See Hut Booked List',
+        'setting' => 'Update Setting of SMTP and Web Site',
+        'tesimonial' => 'Manage All Testimonial',
+        'report' => 'Booking Report',
+        'gallery' => 'Manage Gallery page',
+        'contact' => 'Contact Message Page',
+        'roles' => 'Manage Roles and Permissions',
+    ]
+];
