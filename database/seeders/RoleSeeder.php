e4e13e1da30a5460f6d04acd43d6d96b574a7267
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-21 22:47:29 +0100

    Create RoleSeeder.php

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        // Define the permissions array
+        $permissions = ["dashboard", "team", "bookarea", "huts", "booking", "hutlist", "setting", "tesimonial", "report", "gallery", "contact", "roles"];
+
+        // Create roles and assign permissions
+        Role::create([
+            'name' => 'SuperAdmin',
+            'permissions' => json_encode($permissions),
+        ]);
+    }
+}
