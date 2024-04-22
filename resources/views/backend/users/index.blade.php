91baab1f7e1b62db3b869704f55a9de7e525c4e1
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-23 00:16:57 +0100

    Create index.blade.php

---
+++ resources/views/backend/users/index.blade.php
@@ -0,0 +1,63 @@
+@extends('admin.admin_dashboard')
+@section('admin')
+
+<div class="page-content">
+    <!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+        <div class="ps-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 p-0">
+                    <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">All Users</li>
+                </ol>
+            </nav>
+        </div>
+
+        <div class="ms-auto">
+            <div class="btn-group">
+                <a href="{{ route('add.users') }}" class="btn btn-primary px-5">Add Users </a>
+
+            </div>
+        </div>
+    </div>
+    <!--end breadcrumb-->
+    <hr />
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="example" class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Sl</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $key=> $item)
+                        <tr>
+                            <td>{{ $key+1 }}</td>
+                            <td><img src="{{ asset('upload/user_images/'.$item->photo) }}" alt="" style="width:70px; height:40px;"></td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->email }}</td>
+                            <td>{{ $item->roles->name }}</td>
+                            <td>
+                                <a href="{{ route('edit.user',$item->id) }}" class="btn btn-warning rounded-pill px-4 mx-1">Edit</a>
+                                <a href="{{ route('delete.user',$item->id) }}" class="btn btn-danger rounded-pill px-4 mx-1" id="delete">Delete</a>
+                            </td>
+                        </tr>
+                        @endforeach
+
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
