c399463e9293e892138ad47db5958f2013b40d9f
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-22 16:38:03 +0100

    Update edit_profile.blade.php

--- resources/views/frontend/dashboard/edit_profile.blade.php
+++ resources/views/frontend/dashboard/edit_profile.blade.php
@@ -89,7 +89,7 @@
                                                     </div>
                                                 </div>
 
-                                                <button type="submit" class="btn btn-danger">Save Changes </button>
+                                                <button type="submit" class="btn" style="background-color:  #595f39;">Save Changes </button>
                                             </div>
                                         </div>
                                     </div>
