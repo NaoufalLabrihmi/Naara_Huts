e50209a9d5b4d4a065e5ac35be2ae1faba767034
Naoufal <naoufal.labrihmi@gmail.com>
2024-04-22 16:37:21 +0100

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -13,34 +13,36 @@ use App\Http\Controllers\Auth\ConfirmPasswordController;
 
 Auth::routes();
 Route::group(['namespace' => 'App\Http\Controllers\Auth'], function () {
-    // ----------------------------login ------------------------------//
-    Route::controller(LoginController::class)->group(function () {
-        Route::get('login', 'login')->name('login');
-        Route::post('login/push', 'authenticate')->name('login/push');
-        Route::get('/logout', 'logout')->name('logout');
-        Route::post('change/password', 'changePassword')->name('change/password');
-    });
+    Route::middleware('guest')->group(function () {
+        // ----------------------------login ------------------------------//
+        Route::controller(LoginController::class)->group(function () {
+            Route::get('login', 'login')->name('login');
+            Route::post('login/push', 'authenticate')->name('login/push');
+            Route::get('/logout', 'logout')->name('logout');
+            Route::post('change/password', 'changePassword')->name('change/password');
+        });
 
-    // ------------------------ register sccount ----------------------//
-    Route::controller(RegisterController::class)->group(function () {
-        Route::get('register/form', 'index')->name('register/form');
-        Route::post('register/save', 'saveRecord')->name('register/save');
-    });
+        // ------------------------ register sccount ----------------------//
+        Route::controller(RegisterController::class)->group(function () {
+            Route::get('register/form', 'index')->name('register/form');
+            Route::post('register/save', 'saveRecord')->name('register/save');
+        });
 
-    // -------------------------- forgot password ---------------------//
-    Route::controller(ForgotPasswordController::class)->group(function () {
-        Route::get('/forgot/password', 'sendEmail')->name('forgot/password');
-        Route::post('post/email', 'postEmail')->name('post/email');
-    });
+        // -------------------------- forgot password ---------------------//
+        Route::controller(ForgotPasswordController::class)->group(function () {
+            Route::get('/forgot/password', 'sendEmail')->name('forgot/password');
+            Route::post('post/email', 'postEmail')->name('post/email');
+        });
 
-    // ------------------------- reset password -----------------------//
-    Route::controller(ResetPasswordController::class)->group(function () {
-        Route::get('reset/password/{token}', 'getPassword');
-        Route::post('reset/password', 'updatePassword')->name('reset/password');
-    });
+        // ------------------------- reset password -----------------------//
+        Route::controller(ResetPasswordController::class)->group(function () {
+            Route::get('reset/password/{token}', 'getPassword');
+            Route::post('reset/password', 'updatePassword')->name('reset/password');
+        });
 
-    // ------------------------ confirm password -----------------------//
-    Route::controller(ConfirmPasswordController::class)->group(function () {
-        Route::get('confirm/password', 'confirmPassword')->name('confirm/password');
+        // ------------------------ confirm password -----------------------//
+        Route::controller(ConfirmPasswordController::class)->group(function () {
+            Route::get('confirm/password', 'confirmPassword')->name('confirm/password');
+        });
     });
 });
